2b299ba924ef44e2701ee6af5b0029210f95bae6
kimhaun <wha02068@gmail.com>
2021-02-21 14:43:47 +0900

    입력한 닉네임 들고 메인 복귀  => 메인에서는 아직 반영 X

--- app/src/main/java/kr/co/neoplus/intent_20210221/EditNicknameActivity.kt
+++ app/src/main/java/kr/co/neoplus/intent_20210221/EditNicknameActivity.kt
@@ -1,11 +1,33 @@
 package kr.co.neoplus.intent_20210221
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        okBtn.setOnClickListener {
+//            입력한 닉네임 전달 => 입력한 변수로 저장
+            val inputNickname = newNicknameEdt.text.toString()
+//            확인버튼을 누른게 맞다 명시 + 그 때 들고갈 데이터 첨부
+
+//            데이터 첨부 전용 intent 생성
+//            복귀용이모르 출발/도착지 정보 필요 없음 빈칸 유지
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickName)
+
+//            확인버튼을 누른게 맞다 명시 + 그때 들고갈 데이터 첨부
+//            이 화면에 온 결과를 세팅
+            setResult(Activity.RESULT_OK,resultIntent)
+
+//            실제로 이전화면으로 복귀
+            finish()
+
+
+        }
     }
 }
