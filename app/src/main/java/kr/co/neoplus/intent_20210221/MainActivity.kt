a4ec27911fb8c3ff7578a2140be6c04056639502
kimhaun <wha02068@gmail.com>
2021-02-21 13:58:10 +0900

    전달 받은 데이터 활용 예제

--- app/src/main/java/kr/co/neoplus/intent_20210221/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/intent_20210221/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(myIntent)
 
-
         }
     }
 }
--- app/src/main/java/kr/co/neoplus/intent_20210221/SecondActivity.kt
+++ app/src/main/java/kr/co/neoplus/intent_20210221/SecondActivity.kt
@@ -2,10 +2,18 @@ package kr.co.neoplus.intent_20210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+//        목적지에 도착한 상황 => 첨부한 데이터를 받아주자 (어떤 형태에 자료인가?)
+                 val receivedMessage = intent.getStringExtra("message")
+
+//        수신문구 표현 텍스트뷰의 문구로 설정 => 상황마다 코드가 달라짐
+        receivedMessageTxt.text = receivedMessage
+
     }
 }
